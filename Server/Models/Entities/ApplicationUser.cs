f461dd5c28926a979565ed2f77bc4e2024cea406
Eli Sipos <sipose@students.westerntc.edu>
2025-03-20 12:26:44 -0500

    inital commit

---
+++ Server/Models/Entities/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace PlotPocket.Server.Models.Entities;
+
+public class ApplicationUser : IdentityUser {
+  public virtual ICollection<Show> Shows { get; set; } = new List<Show>();
+}
---
+++ Server/Models/Responses/MovieResponse.cs
@@ -0,0 +1,33 @@
+using System.Text.Json.Serialization;
+
+namespace PlotPocket.Server.Models.Responses;
+
+public class MovieResponse {
+    /*
+        TODO: 
+        Define the model based on the response from the Movie endpoints from
+        The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
+        above each of your C# properties to ensure that the JSON maps properly to your objects.
+    */
+}
+
+public class Date {
+   /*
+			  This model is used on the Movie's "Now Playing" endpoint.
+			  
+		   
+        TODO: 
+        Define the model based on the response from the Movie endpoints from
+        The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
+        above each of your C# properties to ensure that the JSON maps properly to your objects.
+    */
+}
+
+public class Movie : ApiMediaItem {
+   /*
+        TODO: 
+        Define the model based on the response from the Movie endpoints from
+        The Movie Database's API. Be sure to use the [JsonPropertyName("<property>")] attribue
+        above each of your C# properties to ensure that the JSON maps properly to your objects.
+    */
+}
