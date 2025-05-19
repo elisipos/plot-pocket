968491afe54dd3adf860a6d23fafc8c486b3ed95
Eli Sipos <elijsipos@gmail.com>
2025-05-19 13:00:49 -0500

    crude but working details page is finished.

--- Client/src/app/components/media-details/media-details.component.ts
+++ Client/src/app/components/media-details/media-details.component.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { MoviesService } from '../../services/movies.service';
 import { MediaItem } from '../../models/media-item';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-media-details',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './media-details.component.html',
   styleUrl: './media-details.component.css'
 })
@@ -17,6 +18,19 @@ export class MediaDetailsComponent implements OnInit{
   public show!: MediaItem;
 
   constructor(private route: ActivatedRoute) {}
+
+  public convertToHrsMin(time: number): string {
+    let minutes = time % 60;
+    let hours = (time - minutes) / 60;
+
+    return `${hours}h ${minutes}m`;
+  }
+
+  public formatUSD(input: number): string {
+    const USDFormatter = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 0, maximumFractionDigits: 0})
+
+    return USDFormatter.format(input);
+  }
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
--- Client/src/app/models/media-item.ts
+++ Client/src/app/models/media-item.ts
@@ -1,3 +1,5 @@
+import { Genre } from "./genre";
+
 export interface MediaItem {
   adult: boolean;
   backdropPath: string;
@@ -8,12 +10,16 @@ export interface MediaItem {
   overview: string;
   posterPath: string;
   type: string;
-  genreIds: number[];
+  genres: Genre[];
   popularity: number;
   date: string;
   video: boolean;
   voteAverage: number;
   voteCount: number;
+  tagline: string;
+  budget: number;
+  homepage: string;
+  runtime: number;
 
   showApiId: number;
 }
