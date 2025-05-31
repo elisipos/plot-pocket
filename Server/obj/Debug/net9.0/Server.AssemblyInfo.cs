3e3e5dccfc9340a95b77073626b5b02747350181
Eli Sipos <elijsipos@gmail.com>
2025-05-31 14:27:20 -0500

    server.csproj change

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed1dbb70959c7b3189d05796d10c2a90ce5b496c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6cc84a3820de46ee8563ae0de1a7620b38441fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
