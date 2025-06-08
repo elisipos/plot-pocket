e50f00ae17460d65b9c5042e98991497a94961be
Eli Sipos <elijsipos@gmail.com>
2025-06-08 10:31:25 -0500

    added an expiration to each localStorage login. Now logs the front end out after 15 minutes, which is Renders inactivity spin down time.

--- Client/src/app/auth/services/auth.service.ts
+++ Client/src/app/auth/services/auth.service.ts
@@ -18,7 +18,12 @@ export class AuthService {
   constructor() {
     // attempt to get user from localStorage
     const userJsonRaw = localStorage.getItem(this._userKey);
-    const user: User | null = userJsonRaw ? JSON.parse(userJsonRaw) : null;
+    let user: User | null = userJsonRaw ? JSON.parse(userJsonRaw) : null;
+    const now = new Date().getTime();
+    // if expired, set user object to null.
+    if(user){
+      user = user?.expiry < now ? null : user;
+    }
     this._userSubject.next(user);
   }
 
@@ -35,18 +40,25 @@ export class AuthService {
   public login(details: EmailLoginDetails): Observable<User> {
     return this._http.post<User>(`${environment.apiUrl}/auth/login`, details, {
       withCredentials: true
-    })
-      .pipe(tap(user => {
-        localStorage.setItem(this._userKey, JSON.stringify(user));
-        this._userSubject.next(user);
+      }
+    ).pipe(tap(user => {
+        const expiryDate = new Date().getTime() + 900; // 15 minutes, Render's inactivity spin down time.
+        let userWithExpiration = {
+          id: user.id,
+          email: user.email,
+          expiry: expiryDate
+        }
+
+        localStorage.setItem(this._userKey, JSON.stringify(userWithExpiration));
+        this._userSubject.next(userWithExpiration);
       }))
   }
 
   public logout(): Observable<any> {
     return this._http.post<any>(`${environment.apiUrl}/auth/logout`, {}, {
       withCredentials: true
-    })
-      .pipe(tap(() => {
+      }
+    ).pipe(tap(() => {
         localStorage.removeItem(this._userKey);
         this._userSubject.next(null);
       }))
--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98726f41375e1854558c71c3da62caa0f86b31f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ed3e0dfa465be09cea60ff8bbd00b1dba3ea076")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
